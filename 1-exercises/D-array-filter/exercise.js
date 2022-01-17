01a84c9271e8342949a4198ba7edab49a153dc91
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-01-17 10:48:50 +0000

    Update exercise.js

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,8 +8,9 @@
 
 let pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-let pairsByIndex =
-  pairsByIndexRaw.filter((name) => !pairsByIndexRaw.includes(name)).length == 0;
+let pairsByIndex = pairsByIndexRaw.filter(
+  (name) => Array.isArray(name) && name.length == 2
+);
 
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
