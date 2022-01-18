a44a3e49c74e667bee3c8c8459c69eef91dffee9
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-01-18 14:23:08 +0000

    Update 1-create-functions.js

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -3,16 +3,15 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the first five elements of the passed array.
 */
-function first5() {
-}
+
+const first5 = (newarray) => newarray.slice(0, 5);
 
 /*
 Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the same elements, except sorted.
 */
-function sortArray() {
-}
+const sortArray = (newarray) => [...newarray].sort();
 
 /*
 NOTE: This exercise is the same as one you did last week - try to do it again using things you learnt this week.
@@ -24,8 +23,8 @@ Write a function that:
 - Removes any forward slashes (/) in the strings.
 - Makes the strings all lowercase.
 */
-function tidyUpString() {
-}
+const tidyUpString = (newarray) =>
+  newarray.map((m) => m.trim().replace(/\//g, "").toLowerCase());
 
 /*
 Write a function that:
@@ -33,8 +32,7 @@ Write a function that:
 - Returns a new array containing the same elements, but without the element at the passed index.
 */
 
-function remove() {
-}
+function remove() {}
 
 /*
 Write a function that:
@@ -44,8 +42,7 @@ Write a function that:
 - Numbers greater 100 must be replaced with 100.
 */
 
-function formatPercentage() {
-}
+function formatPercentage() {}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
