76b33d5d614cedbc43c792d278d7e72b659542af
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2022-01-19 23:35:02 +0000

    Add E-array-map solution.

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -3,9 +3,27 @@
 
 let numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
-let numbersMultipliedByOneHundred; // complete this statement
+// Solution 1
+let numbersMultipliedByOneHundred = numbers.map(number => number * 100);
+
+// Solution 2
+let numbersMultipliedByOneHundred0 = numbers.map(number => {
+  return number * 100;
+});
+// Solution 3
+let numbersMultipliedByOneHundred1 = numbers.map(function (number) {
+  return number * 100;
+});
+
+// Solution 4
+let numbersMultipliedByOneHundred2 = numbers.map(function Double(number) {
+  return number * 100;
+});
 
 console.log(numbersMultipliedByOneHundred);
+console.log(numbersMultipliedByOneHundred0);
+console.log(numbersMultipliedByOneHundred1);
+console.log(numbersMultipliedByOneHundred2);
 
 /* EXPECTED RESULT
 
