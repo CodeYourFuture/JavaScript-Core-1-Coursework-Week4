7d7a7bdc3738f2c1c4431ae370eed78e84287654
Selcuk Karakus <selcukkarakus@Selcuks-MBP.lan>
2022-01-20 15:46:46 +0000

    Add mandatory-1 solutions.

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -3,16 +3,18 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the first five elements of the passed array.
 */
-function first5() {
-}
+const first5 = (arrayOfNumbers) => {
+  return arrayOfNumbers.slice(0, 5);
+};
 
 /*
 Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the same elements, except sorted.
 */
-function sortArray() {
-}
+const sortArray = (arrayOfLetters) => {
+  return arrayOfLetters.slice().sort();
+};
 
 /*
 NOTE: This exercise is the same as one you did last week - try to do it again using things you learnt this week.
@@ -24,8 +26,11 @@ Write a function that:
 - Removes any forward slashes (/) in the strings.
 - Makes the strings all lowercase.
 */
-function tidyUpString() {
-}
+const tidyUpString = (arrayOfStrings) => {
+  return arrayOfStrings.map((string) => {
+    return string.trim(" ").replace("/", "").toLowerCase();
+  });
+};
 
 /*
 Write a function that:
@@ -33,8 +38,11 @@ Write a function that:
 - Returns a new array containing the same elements, but without the element at the passed index.
 */
 
-function remove() {
-}
+const remove = (arrayItems, index) => {
+  let clonedArray = arrayItems.splice("");
+  clonedArray.splice(index, 1);
+  return clonedArray;
+};
 
 /*
 Write a function that:
@@ -44,8 +52,13 @@ Write a function that:
 - Numbers greater 100 must be replaced with 100.
 */
 
-function formatPercentage() {
-}
+const formatPercentage = (arrayOfNumbers) => {
+  return arrayOfNumbers
+    .map((number) => {
+      return number > 100 ? 100 : number;
+    })
+    .map((number) => `${Math.round(number * 100) / 100}%`);
+};
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
