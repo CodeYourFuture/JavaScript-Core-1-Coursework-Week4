8244215a8aed2aa3045a23863042af58d502a2b1
Hamra Khan <hamra.khan.b@gmail.com>
2022-01-20 01:06:46 +0000

    completed exercises H - J

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  return str.split(" ").map( function () {
+    return str.charAt(0).toUpperCase() + str.substring(1);
+  }).join("");
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
