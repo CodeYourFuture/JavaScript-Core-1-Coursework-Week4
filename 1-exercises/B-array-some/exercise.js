d4e002160b1ad8bf4ac9eeba9e9c54c5406bdb60
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-01-21 21:23:30 +0000

    Update exercise.js

--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -5,6 +5,9 @@
   - Add a check for null values, and if one exists, exit the program
   - Do not edit any of the existing code
 */
+function nullValue(element) {
+ return element === null;
+}
 
 let pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 
@@ -14,6 +17,9 @@ let pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
+
+if (pairsByIndex.some(nullValue))
+     process.exit(1);
 
 let pairs = pairsByIndex.map(function (indexes) {
   let student = students[indexes[0]];
