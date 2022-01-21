2548b1401b09902a8e061e43810ab56ea8fb1aa1
Harsheek Thanki <harsheekthanki@gmail.com>
2022-01-21 18:55:56 +0000

    exercise F complete

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,18 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(function (element) {
+  if (element % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (element % 5 === 0) {
+    console.log("Buzz");
+  } else if (element % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(element);
+  }
+});
+
 /* EXPECTED OUTPUT */
 
 /*
