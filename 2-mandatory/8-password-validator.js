73fc481b5f51550610f375be03f3e2265294c654
Harsheek Thanki <harsheekthanki@gmail.com>
2022-01-25 23:13:24 +0000

    progress on password validator

--- 2-mandatory/8-password-validator.js
+++ 2-mandatory/8-password-validator.js
@@ -23,7 +23,24 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {}
+function isValidPasswords(passwords) {
+    return (containsAtLeastFiveCharacters(password) && containsUppercaseLetter(password) && containsLowercaseLetter(password) && containsNumber(password) &&
+      containsSymbol(password));
+}
+
+function validatePasswords(passwords) {
+  let seenPasswords = [];
+  let validatedPasswords = passwords.map((password) => {
+    if (seenPasswords.includes(password)) {
+      return false;
+    }
+     seenPasswords.push(password);
+     return isValidPassword(password);
+  }
+
+function containsAtLeastFiveCharacters(string) {
+  return string.length >= 5;
+}
 
 // Returns true if string contains at least one uppercase letter.
 function containsUppercaseLetter(string) {
