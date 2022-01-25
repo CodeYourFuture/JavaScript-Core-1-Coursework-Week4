1f717467fd54a3d461aec031cb35cab7ee3fa0d7
Harsheek Thanki <harsheekthanki@gmail.com>
2022-01-25 23:22:09 +0000

    all mandatory exercises complete

--- 2-mandatory/8-password-validator.js
+++ 2-mandatory/8-password-validator.js
@@ -23,20 +23,29 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function isValidPasswords(passwords) {
-    return (containsAtLeastFiveCharacters(password) && containsUppercaseLetter(password) && containsLowercaseLetter(password) && containsNumber(password) &&
-      containsSymbol(password));
+function isValidPasswords(password) {
+  return (
+    containsAtLeastFiveCharacters(password) &&
+    containsUppercaseLetter(password) &&
+    containsLowercaseLetter(password) &&
+    containsNumber(password) &&
+    containsSymbol(password)
+  );
 }
 
 function validatePasswords(passwords) {
   let seenPasswords = [];
+
   let validatedPasswords = passwords.map((password) => {
     if (seenPasswords.includes(password)) {
       return false;
     }
-     seenPasswords.push(password);
-     return isValidPassword(password);
+    seenPasswords.push(password);
+    return isValidPasswords(password);
   });
+
+  return validatedPasswords;
+}
 
 function containsAtLeastFiveCharacters(string) {
   return string.length >= 5;
