e0a58c995ada6855f5301df27dcfd836c5e29910
Timea <timea.reich@gmail.com>
2022-04-30 00:50:27 +0100

    extra done

--- 3-extra/2-sorting-algorithm.js
+++ 3-extra/2-sorting-algorithm.js
@@ -5,17 +5,38 @@ numbers, in ascending or descending order.
 Today, you will be applying the sorting algorithm you used in that exercise in code! 
 
 Create a function called sortAges which:
-- takes an array of mixed data types as input
+- takes an arr of mixed data types as input
 - removes any non-number data types without using the built-in javascript filter method
-- returns an array of sorted ages in ascending order 
+- returns an arr of sorted ages in ascending order 
   - HARD MODE - without using the built-in javascript sort method 😎
 
 You don't have to worry about making this algorithm work fast! The idea is to get you to
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
-
+function sortAges(arr) {
+  let numberArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Number.isInteger(arr[i])) {
+      numberArr.push(arr[i]);
+    }
+  }
+  let done = false;
+  while (!done) {
+    console.log(done);
+    done = true;
+    console.log(done);
+    for (let i = 1; i < numberArr.length; i++) {
+      if (numberArr[i - 1] > numberArr[i]) {
+        done = false;
+        let tmp = numberArr[i - 1];
+        numberArr[i - 1] = numberArr[i];
+        numberArr[i] = tmp;
+      }
+    }
+  }
+  return numberArr;
+}
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const agesCase1 = [
@@ -36,6 +57,7 @@ const agesCase1 = [
 ];
 const agesCase2 = ["28", 100, 60, 55, "75", "🍕", "Elamin"];
 
+console.log(sortAges(agesCase1));
 test("sortAges function works - case 1", () => {
   expect(sortAges(agesCase1)).toEqual([23, 45, 55, 66, 100]);
 });
