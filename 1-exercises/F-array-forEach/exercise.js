5fc4f9a9a03a73c5e9adfbdeb139a756c98f2d82
Timea <timea.reich@gmail.com>
2022-04-30 00:56:07 +0100

    updated F-array-foreach

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,17 +9,11 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-
-arr.forEach(function(num){
-  if (num%3===0 && num%5===0)
-    num='FizzBuzz';
-    else if (num%3===0)
-    num='Fizz';
-    else if (num%5===0)
-    num='Buzz';
-    console.log(num)
+arr.forEach(function (num) {
+  if (num % 3 === 0 && num % 5 === 0) num = "FizzBuzz";
+  else if (num % 3 === 0) num = "Fizz";
+  else if (num % 5 === 0) num = "Buzz";
 });
-
 
 // function fizz(num){
 //   if (num%3===0 && num%5===0){
@@ -33,8 +27,6 @@ arr.forEach(function(num){
 //   }
 //   return num
 // }
-
-
 
 /* EXPECTED OUTPUT */
 
