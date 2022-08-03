503009bd37cc17ee70c44ef6761c9a786c17332a
chandredewet <chandredewet@gmail.com>
2022-08-03 18:30:07 +0200

    Completed Exercise 5

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,15 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+function multiple(num) {
+  if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
+  else if (num % 5 === 0) return "Buzz";
+  else if (num % 3 === 0) return "Fizz";
+  else return num;
+}
+
+arr.map(multiple).forEach((output) => console.log(`${output}`));
+
 /* EXPECTED OUTPUT */
 
 /*
