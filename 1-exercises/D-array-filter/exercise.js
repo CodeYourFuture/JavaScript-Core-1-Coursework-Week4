f7fe4821c5fd534a00b689a3c3c34057806ec8ab
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2022-08-03 11:47:56 +0200

    Complete exercise D

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,7 +8,13 @@
 
 let pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-let pairsByIndex; // Complete this statement
+function findIndex(element) {
+  if (typeof element === "object" && element !== null) {
+    return element.length == 2;
+  }
+}
+
+let pairsByIndex = pairsByIndexRaw.filter(findIndex);// Complete this statement
 
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
