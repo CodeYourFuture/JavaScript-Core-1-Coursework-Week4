cdb97618d92697f8d3f8536a4a35e6828e05f8a8
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-08-11 00:07:51 +0200

    Update 8-password-validator.js

--- 2-mandatory/8-password-validator.js
+++ 2-mandatory/8-password-validator.js
@@ -23,7 +23,26 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {}
+function validatePasswords(passwords) {
+  passwords = passwords.map((element, index) => {
+    if (
+      containsLowercaseLetter(element) &&
+      containsUppercaseLetter(element) &&
+      containsNumber(element) &&
+      containsSymbol(element) &&
+      element.length >= 5 &&
+      index === passwords.indexOf(element)
+    ) {
+      return true;
+    } else return false;
+  });
+  return passwords;
+}
+
+// New function created that returns true if all conditions mentioned below are met.
+function checkForAll(string) {
+  return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,16}$/.test(string);
+}
 
 // Returns true if string contains at least one uppercase letter.
 function containsUppercaseLetter(string) {
