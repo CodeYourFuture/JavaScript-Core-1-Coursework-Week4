7aa27834859f950d8e3eee520fdb2a82d75efa67
Kawa MANMI <kawamam2007@gmail.com>
2022-09-05 22:58:05 +0100

    exercise E has been done

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,22 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(function (elem) 
+{if (elem % 3 ===0 && elem % 5 ===0) {
+  console.log("FizzBuzz");
+  }
+  else if (elem % 3 ===0) {
+  console.log("Fizz");
+  }
+else if (elem % 5 ===0) {
+   console.log("Buzz");
+  }
+else {
+  console.log(elem)
+  }
+});
+
+
 /* EXPECTED OUTPUT */
 
 /*
