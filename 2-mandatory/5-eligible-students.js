eede748e529fe186a14954fe6588cc0de0b50be1
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-09-05 23:45:12 +0100

    Solution ex5

--- 2-mandatory/5-eligible-students.js
+++ 2-mandatory/5-eligible-students.js
@@ -7,7 +7,14 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function getEligibleStudents() {}
+function getEligibleStudents(array) {
+  let newArray = array.filter((elt) => elt[1] >= 8);
+  let studentsArray = [];
+  for (let arr of newArray) {
+    studentsArray.push(arr[0]);
+  }
+  return studentsArray;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
