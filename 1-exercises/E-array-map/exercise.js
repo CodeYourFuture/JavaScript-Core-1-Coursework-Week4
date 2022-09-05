657186a264b4fb9dad576dd3a5afe76183390b34
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-05 14:17:24 +0100

    Update exercise.js

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -3,7 +3,7 @@
 
 let numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
-let numbersMultipliedByOneHundred; // complete this statement
+let numbersMultipliedByOneHundred = numbers.map(number => number * 100); // complete this statement
 
 console.log(numbersMultipliedByOneHundred);
 
