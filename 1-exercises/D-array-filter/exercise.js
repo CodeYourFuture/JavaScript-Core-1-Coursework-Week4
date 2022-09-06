ebd53e4d1d0bd8d26fa1eb70fba98ff70f41ff81
Chelle <dearmichelley@gmail.com>
2022-09-06 18:42:30 +0100

    excersises completed

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,7 +8,8 @@
 
 let pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-let pairsByIndex; // Complete this statement
+let pairsByIndex = pairsByIndexRaw.filter(arr => arr && arr.length === 2); // Complete this statement
+//console.log(pairsByIndex)
 
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
