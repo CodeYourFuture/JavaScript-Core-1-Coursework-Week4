4ad0b046575887ae17669d51dc40f0a5ffebb9c8
simeonbikov <bikovsem@gmail.com>
2022-09-07 16:09:39 +0100

    mandatory ex.1

---
+++ 2-mandatory/test.js
@@ -0,0 +1,35 @@
+["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"];
+
+
+
+
+// function formatPercentage(arr) {
+//   return arr.map((e) => e.toFixed(2).concat("%"));
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(formatPercentage(arr));
+
+// function remove(arr, i) {
+//   return arr.filter((e) => e != arr[i]);
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(remove(arr, 6));
+
+// function first5(arr) {
+//     let i = 0;
+//   return arr.slice(0, 5);
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(first5(arr));
+
+// function tidyUpString(arr) {
+//   return arr.map((e) => e.trim().replaceAll("/", "").toLowerCase());
+// }
+
+// let arr = [" Fsd/fg", "fgh", "df/g ", "Fgh", "sd", "Fuhhs", " Sjdyg"];
+
+// console.log(tidyUpString(arr));
