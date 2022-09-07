a62c9c922c547f012ab63edd12225143bed6db98
Pakize Bozkurt <pakiozihak@gmail.com>
2022-09-07 14:24:02 +0100

    Completed A-array-find

--- 1-exercises/A-array-find/exercise.js
+++ 1-exercises/A-array-find/exercise.js
@@ -16,8 +16,11 @@ let names = [
   "Karim",
   "Ahmed",
 ];
+function findNameThatStartsWithA(names) {
+  return names.length > 7;
+}
 
-let longNameThatStartsWithA = findLongNameThatStartsWithA(names);
+let longNameThatStartsWithA = names.find(findNameThatStartsWithA);
 
 console.log(longNameThatStartsWithA);
 
