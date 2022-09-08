e5fc1862080dc5d6b1aaf11fc721a97920a28b77
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-09-08 06:52:23 +0100

    a

--- 1-exercises/A-array-find/exercise.js
+++ 1-exercises/A-array-find/exercise.js
@@ -2,7 +2,9 @@
   You are given an array of names.
   Using .find(), we'd like to find the first name which starts with A and is longer than 7 letters.
 */
-
+function findLongNameThatStartsWithA(names) {
+  return names.find(element => element.length > 7 && element.startsWith('A'))
+}
 // write your code here
 
 let names = [
