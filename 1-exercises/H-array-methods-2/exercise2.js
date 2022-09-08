fc7fd172a436244a2bdf7c1ac129b666be7a9535
Jade Jones <jadejonescrypto@gmail.com>
2022-09-08 15:16:15 +0100

    array

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,9 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  return([initial, ... rest]) =>[initial.toUpperCase(), ...rest].join("")
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
