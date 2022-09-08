9b9baf9c94ba8aeecefe13ab7fb7d2981e5e8ed5
AzinYad <103781572+AzinYad@users.noreply.github.com>
2022-09-08 18:51:04 +0100

    exercises

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -1,13 +1,15 @@
 /*
   Array methods - .join()
   -------------------------
-  Complete the capitalise function 
+  Complete the capitalize function 
   It should return a string with the first letter in uppercase
   For example, capitailise("hello") should return "Hello"
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+ return  str.charAt(0).toUpperCase()+str.slice(1);
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
