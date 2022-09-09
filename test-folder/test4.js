bada9e87b8ef76b8da7fd4c46b141d543128cf6d
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2022-09-09 16:04:59 +0100

    complete all mandatory

--- test-folder/test4.js
+++ test-folder/test4.js
@@ -16,4 +16,35 @@
 // console.log(checkCodeIsThere(string2));
 // console.log(checkCodeIsThere(string3));
 
+function getTransportModes(arr) {
+  return arr.slice(1, arr.length)
+}
 
+// console.log(getTransportModes([1,2,3,4,5,6]))
+
+function isAccessibleByTransportMode(arr,element) {
+    return arr.some(item => item === element);
+}
+
+// console.log(isAccessibleByTransportMode([1,2,4], 'boat'))
+
+function getLocationName(arr) {
+  return arr.slice(0, 1);
+}
+
+// console.log(getLocationName(["Tower Bridge", "tube", "river boat"]))
+
+function journeyPlanner(locations, transportMode) {
+  let finalArray =  locations.map(item=>{
+    let transModes = getTransportModes(item);
+    if (isAccessibleByTransportMode(transModes,transportMode)) 
+    return getLocationName(item).toString()
+  })
+  return finalArray.filter(element => {return element !== undefined});
+}
+
+console.log(journeyPlanner([
+         ["Angel", "tube", "bus"],
+         ["London Bridge", "tube", "river boat"],
+         ["Walsall","bus"]
+       ], 'bus'))
