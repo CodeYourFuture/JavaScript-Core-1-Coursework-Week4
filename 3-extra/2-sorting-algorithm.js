22808db1783f8a3b58ece53aee94235b4af0fad5
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2022-09-09 15:46:19 +0100

    ExtraExercise_Added

--- 3-extra/2-sorting-algorithm.js
+++ 3-extra/2-sorting-algorithm.js
@@ -14,7 +14,13 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+function sortAges(arr) {
+  let array = [];
+  for (let x of arr) {
+    if (typeof x === "number") array.push(x);
+  }
+  return array.sort((a, b) => a - b);
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
