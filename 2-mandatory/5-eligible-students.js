b171a69eed0e663c8b903570bc7181a8bcdfb38f
Alex Phillip <alexjoelphillip@gmail.com>
2022-09-09 13:49:01 +0100

    completed mandatory 5

--- 2-mandatory/5-eligible-students.js
+++ 2-mandatory/5-eligible-students.js
@@ -7,7 +7,14 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function getEligibleStudents() {}
+function getEligibleStudents(arr) {
+  let eligibleStudents = [];
+  arr.filter(student => {if (student[1] > 7) {
+  eligibleStudents.push(student[0])};
+  })
+  return eligibleStudents;
+  }
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
