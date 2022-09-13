f70bbf9efb71dc831a7fb857aeabae5da2ae5e30
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2022-09-13 16:14:43 +0100

    complete all extra

--- 3-extra/card-validator.js
+++ 3-extra/card-validator.js
@@ -7,7 +7,38 @@
 
 
 function validateCreditCardNumber(number) {
-       return !isNaN(number) && number.charAt(number.length -1) & 2 === 0 ;    
+       function getSum(number) { //this function sums the digits in the number
+              return String(number).split('').reduce((acc, curr) => {return acc + Number(curr);},0)
+       };
+       function checkSame(number) { //this function checks that number has at least 2 different digits
+              var digit = number % 10;
+              while (number != 0) {
+                     var currentDigit = number % 10;
+                     number = parseInt(number/10);
+                     if (currentDigit != digit) {
+                            return false
+                     }
+              }
+              return true
+       }
+       // console.log(checkSame(number))
+       // console.log(getSum(number))
+      if (!isNaN(number) && (number&2===0)===0 && number.toString().length === 16 && getSum(number)>16 && checkSame(number)===false) {
+       return true
+}
+else return false    
 }
 
-console.log(validateCreditCardNumber("23434"));
+let number1 = 1234567890123456
+let number2 = 11111111111111125
+let number3 = 2222222222222222
+let number4 = "1234567890123456"
+let number5 = 1000064700000006
+let number6 = 0000000000300003
+
+console.log(`${number1} is ${validateCreditCardNumber(number1)}`);
+console.log(`${number2} is ${validateCreditCardNumber(number2)}`)
+console.log(`${number3} is ${validateCreditCardNumber(number3)}`)
+console.log(`${number4} is ${validateCreditCardNumber(number4)}`)
+console.log(`${number5} is ${validateCreditCardNumber(number5)}`)
+console.log(`${number6} is ${validateCreditCardNumber(number6)}`)
