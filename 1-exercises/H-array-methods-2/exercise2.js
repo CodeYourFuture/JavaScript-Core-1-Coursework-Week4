eb98082cb281feecddc7fa295ebda35276e2263b
Mohammadreza <104798944+Mohammadrezanafar@users.noreply.github.com>
2022-09-28 00:20:16 +0100

    exercise task

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  let split = str.split("");
+	split[0] = split[0].toUpperCase();
+	return split.join("");
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
