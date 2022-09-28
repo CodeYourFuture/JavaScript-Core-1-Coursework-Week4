a3e42dc1e6524008cc4dd933a5de9f5e9fe0f111
Rebwar <anvar.azizi21@gmail.com>
2022-09-28 15:03:51 +0100

    1-exercises/C-array-every

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -4,8 +4,9 @@
 
 let students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 let group = ["Austine", "Dany", "Swathi", "Daniel"];
+// let group = ["Austine", "Dany", "Swathi", "Rukmini"];
 
-let groupIsOnlyStudents; // complete this statement
+let groupIsOnlyStudents = group.every((el) => students.includes(el));
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
