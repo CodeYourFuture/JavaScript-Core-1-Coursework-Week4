ad4afec9a071f9bef889b1e9d215dbc34375ec74
Milad E-Pour <epour.milad@gmail.com>
2022-09-28 19:49:47 +0100

    Exercise Done

--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -7,6 +7,9 @@
 */
 
 let pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
+const checkIf = pairsByIndex.some((index) => index === null);
+if (checkIf) return;
+
 
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
--- 1-exercises/I-string-replace/exercise.js
+++ 1-exercises/I-string-replace/exercise.js
@@ -12,8 +12,14 @@
 
 let story =
   "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
+ 
+let result = story.replace(/dogs/g, "cats")
+.replace(/day/g, "night")
+.replace("10", "100000")
+.replace("great", "brilliant");
 
-let result = story.replace("", "");
+
+
 
 /* EXPECTED OUTPUT */
 
