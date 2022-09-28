ad41560a63fd426bcb811ad9ebb0f935b6148e7d
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2022-09-28 22:06:03 +0100

    done all exercises

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -8,7 +8,10 @@
 */
 
 function capitalise(str) {}
+function capitalise(str) {
 
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
