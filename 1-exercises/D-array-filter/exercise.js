4528d0ee947144e87628ef562301556eac2ef509
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-09-29 23:44:55 +0100

    exercise d filter method

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,7 +8,10 @@
 
 let pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-let pairsByIndex; // Complete this statement
+let pairsByIndex = pairsByIndexRaw.filter(
+  (element) => element !== null && element !== false && typeof element !== "string" && element.length === 2
+);
+console.log(pairsByIndex)
 
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
