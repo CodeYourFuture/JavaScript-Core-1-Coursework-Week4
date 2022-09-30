13f4f7191b8d88966ad86db3027dd31a00d3a8c6
[Luan Murilo de Oliveira e Souza] <[luan.oliveiraesouza@gmail.com]>
2022-09-30 08:26:20 +0100

    answers mandatory

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -3,7 +3,8 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the first five elements of the passed array.
 */
-function first5() {
+function first5(arrayFirstFive) {
+  return arrayFirstFive.slice(0, 5);
 }
 
 /*
@@ -11,7 +12,10 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the same elements, except sorted.
 */
-function sortArray() {
+function sortArray(array) {
+  const newArray = [...array];
+  const arraySorted = newArray.sort();
+  return arraySorted;
 }
 
 /*
@@ -24,7 +28,10 @@ Write a function that:
 - Removes any forward slashes (/) in the strings.
 - Makes the strings all lowercase.
 */
-function tidyUpString() {
+function tidyUpString(cleanArray) {
+  return cleanArray.map((array) => {
+    return array.replace("/", "").trim().toLowerCase();
+  });
 }
 
 /*
@@ -33,7 +40,8 @@ Write a function that:
 - Returns a new array containing the same elements, but without the element at the passed index.
 */
 
-function remove() {
+function remove(array, index) {
+  return array.filter((a, i) => i != index);
 }
 
 /*
@@ -44,7 +52,14 @@ Write a function that:
 - Numbers greater 100 must be replaced with 100.
 */
 
-function formatPercentage() {
+function formatPercentage(number) {
+  return number.map((number) => {
+    if (number > 100) {
+      return "100%";
+    } else {
+      return `${parseFloat(number.toFixed(2))}%`;
+    }
+  });
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
--- 2-mandatory/3-bush-berries.js
+++ 2-mandatory/3-bush-berries.js
@@ -22,7 +22,11 @@
 */
 
 function isBushSafe(berryArray) {
-  //Write your code here
+  if (berryArray.every((colour) => colour === "pink")) {
+    return "Bush is safe to eat from";
+  } else {
+    return "Toxic! Leave bush alone!";
+  }
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
