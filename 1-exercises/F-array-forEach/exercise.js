f10dfadbe58676e6bf4cb40a310956078f8ab898
Jihad Omer <74451984+JihadOmer@users.noreply.github.com>
2022-09-30 16:20:04 +0100

    exeercise is Done

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,20 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+let multiples = arr.forEach((x) => {
+
+  if (x % 5 === 0 && x % 3 ===0) {
+      console.log('“FizzBuzz”');
+  }
+  else if (x % 5 === 0) {
+      console.log('Buzz');
+  }
+  else if (x % 3 ===0) {
+      console.log('Fizz');
+  }
+  else console.log(x);
+})
+
 /* EXPECTED OUTPUT */
 
 /*
