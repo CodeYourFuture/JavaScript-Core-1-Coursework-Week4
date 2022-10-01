9a8112b1065b24948f853fd2f8c615faf1cc1e4d
Rebwar <anvar.azizi21@gmail.com>
2022-10-01 18:52:43 +0100

    reuse the method to remove "not" 1-exercises/J-string-substring/exercise3

--- 1-exercises/J-string-substring/exercise2.js
+++ 1-exercises/J-string-substring/exercise2.js
@@ -20,7 +20,7 @@ names[2] = names[2].substring(0, 4);
 names[3] = names[3].substring(0, 4);
 names[4] = names[4].substring(0, 5);
 
-names.forEach((name) => {
+names.forEach(name => {
   console.log(name);
 });
 
--- 1-exercises/J-string-substring/exercise3.js
+++ 1-exercises/J-string-substring/exercise3.js
@@ -6,14 +6,26 @@
   HINT: You will need to use .substring() twice
 */
 
-let statement = "I do not like programming";
+// let statement = "I do not like programming";
+let statement = "I like programming but I do not like be programmer";
 
 // let result = "";
-let result = statement.substring(0, 4) + statement.substring(8);
-console.log(result);
+// let result = statement.substring(0, 4) + statement.substring(8);
+// console.log(result);
+//💫💫💫💫💫💫💫💫💫💫
 
+function removeNot(str) {
+  let arrStatement = str.split(" ");
+  let indexOfNot = arrStatement.findIndex(e => e === "not");
+  // delete arrStatement[indexOfNot];
+  arrStatement.splice(indexOfNot, 1);
+  return arrStatement.join(" ");
+}
+console.log(removeNot(statement));
 /* EXPECTED OUTPUT
 
   "I do like programming"
 
 */
+//This works well. If you wanted to reuse the method to remove
+//"not" from different sentences, you could find the index of "not".
