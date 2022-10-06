8bd7def55b84e61148150442b86eaca1040318a2
Khesiwe Dube <101154626+KhesiweDube@users.noreply.github.com>
2022-10-06 20:24:12 +0100

    added- using.map method

--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -9,6 +9,8 @@
 let pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 let pairByIndex = pairsByIndexRaw.filter((arr) => arr .length === 2);
 
+
+
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
