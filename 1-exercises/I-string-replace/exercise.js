93dff5d0be1c17610e46c42ebe66343e594dfe13
Khesiwe Dube <101154626+KhesiweDube@users.noreply.github.com>
2022-10-07 13:23:32 +0100

    added, more exercises

--- 1-exercises/I-string-replace/exercise.js
+++ 1-exercises/I-string-replace/exercise.js
@@ -14,6 +14,12 @@ let story =
   "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
 
 let result = story.replace("", "");
+const replacements = {
+  "dogs": "cats",
+  "day": "nights",
+  "10": "100000"
+  ""
+}
 
 /* EXPECTED OUTPUT */
 
