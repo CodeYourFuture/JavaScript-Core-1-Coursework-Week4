aad470b99c5e76caf08b9a5498be47baf00b3ee5
Baz Murphy <bazmurphy@gmail.com>
2022-10-09 20:52:47 +0100

    completed: exercises, mandatory, extra

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,8 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(element => console.log(element % 5 === 0 && element % 3 === 0 ? "FizzBuzz" : element % 5 === 0 ? "Buzz" : element % 3 === 0 ? "Fizz" : element));
+
 /* EXPECTED OUTPUT */
 
 /*
