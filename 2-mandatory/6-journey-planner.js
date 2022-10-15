1100f5c52b1baf5fbbaf9ce7c6cc9b9277d3dcfd
Danti_86 <ajux_86@yahoo.com>
2022-10-15 07:32:29 +0100

    Mandatory\6

--- 2-mandatory/6-journey-planner.js
+++ 2-mandatory/6-journey-planner.js
@@ -19,7 +19,7 @@
 
 function checkCodeIsThere(stringText) {
   let magicWord = "code";
-  //edit code below
+  const containsCode = stringText.includes(magicWord);
   if (stringText) {
     return stringText;
   } else {
@@ -64,7 +64,9 @@ function checkCodeIsThere(stringText) {
 
   Hint: Use the corresponding array method to split the array.
 */
-function getTransportModes() {}
+function getTransportModes(locationDetails) {
+  const transportModes = locationDetails.slice(1);
+  return transportModes;
 
 /*
   Implement the function isAccessibleByTransportMode that
@@ -81,7 +83,10 @@ function getTransportModes() {}
 
   Hint: Use the corresponding array method to decide if an element is included in an array.
 */
-function isAccessibleByTransportMode() {}
+function isAccessibleByTransportMode(transportModes, choosenTransportMode) {
+  return transportModes.includes(choosenTransportMode);
+}
+
 
 /*
   Implement the function getLocationName that
@@ -92,7 +97,9 @@ function isAccessibleByTransportMode() {}
    - Returns the name of the location
       e.g: "Tower Bridge"
 */
-function getLocationName() {}
+function getLocationName(locationDetails) {
+  const locationName = locationDetails[0];
+  return locationName;}
 
 /*
  We arrived at the final method. it won't take long if you use the previously implemented functions wisely.
@@ -122,6 +129,13 @@ function getLocationName() {}
   Advanced challange: try to use arrow function when invoking an array method.
 */
 function journeyPlanner(locations, transportMode) {
+  return locations
+    .filter((location) => {
+      return isAccessibleByTransportMode(location, transportMode);
+    })
+    .map((accessibleLocation) => {
+      return getLocationName(accessibleLocation);
+    });{
   // Implement the function body
 }
 
