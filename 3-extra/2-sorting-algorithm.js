a9f80c629b5d9170bcaf183076c727192d263993
micheale2021 <andemariammicheale@gmail.com>
2022-10-16 22:10:19 +0100

    update sorting algorithm

--- 3-extra/2-sorting-algorithm.js
+++ 3-extra/2-sorting-algorithm.js
@@ -14,7 +14,17 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+function sortAges(arr) {
+  for (let i = 0; i < arr.length; i++){
+      parseInt(arr[i])
+      if(typeof(arr) !== "number")
+      {
+          arr.splice(arr[i], 1);
+          console.log(typeof(arr[i]));
+      }
+  }
+  return arr;
+};  
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
