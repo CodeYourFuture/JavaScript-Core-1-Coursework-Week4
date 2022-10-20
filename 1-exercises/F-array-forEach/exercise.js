a797c50d51f6bcd2469badebf2ecb181484b11db
shafiekdavids <shafiekd@gmail.com>
2022-10-20 18:00:12 +0200

    Excersises

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,18 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+let print = arr.forEach(function (value) {
+  if (value % 3 == 0 && value % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (value % 3 == 0) {
+    console.log("Fizz");
+  } else if (value % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(value);
+  }
+});
+
 /* EXPECTED OUTPUT */
 
 /*
