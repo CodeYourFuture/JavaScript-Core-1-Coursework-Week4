4b82ec84b565a7048406a8d93c9dcb6409eb772a
Anosh Malik <anoshmalik.am@gmail.com>
2022-12-05 19:41:55 +0000

    Finished exercise G

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  let word = str.split("");
+  word[0] = word[0].toUpperCase();
+  return word.join("");
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
