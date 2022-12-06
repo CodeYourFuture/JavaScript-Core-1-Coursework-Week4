4b5629b76da3237b953786e4dd52679c6c8fd23c
Ansu Jammeh <ansuj9@yahoo.com>
2022-12-06 07:04:21 +0000

    stop at mandatory 1-create-function

--- 2-mandatory/1-create-functions.js
+++ 2-mandatory/1-create-functions.js
@@ -3,7 +3,8 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the first five elements of the passed array.
 */
-function first5() {
+function first5(arr) {
+  return arr.slice(0,5)
 }
 
 /*
@@ -11,7 +12,8 @@ Write a function that:
 - Accepts an array as a parameter.
 - Returns a new array containing the same elements, except sorted.
 */
-function sortArray() {
+function sortArray(arr) {
+  return arr.sort()
 }
 
 /*
@@ -24,7 +26,13 @@ Write a function that:
 - Removes any forward slashes (/) in the strings.
 - Makes the strings all lowercase.
 */
-function tidyUpString() {
+function tidyUpString(arr) {
+  return arr.map((string) =>{
+  string = string.trim()
+  string = string.replaceAll("/","")
+  string = string.toLowerCase()
+  return string
+  })
 }
 
 /*
@@ -33,7 +41,10 @@ Write a function that:
 - Returns a new array containing the same elements, but without the element at the passed index.
 */
 
-function remove() {
+function remove(arr,index) {
+  const newArr = [...arr]
+  newArr.splice(index,1)
+  return newArr;
 }
 
 /*
@@ -44,7 +55,15 @@ Write a function that:
 - Numbers greater 100 must be replaced with 100.
 */
 
-function formatPercentage() {
+function formatPercentage(arr) {
+  return arr.map((number)=>{
+if(number > 100) {
+  number = 100;
+}
+number = number.toFixed(2);
+number = number+"%";
+return number
+  })
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
