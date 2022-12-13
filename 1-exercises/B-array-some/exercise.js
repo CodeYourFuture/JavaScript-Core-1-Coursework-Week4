08c2b695d8767b0780ba9e5d185e1ef48b85ff7a
OleksChep <aachep@gmail.com>
2022-12-13 00:03:29 +0000

    B

--- 1-exercises/B-array-some/exercise.js
+++ 1-exercises/B-array-some/exercise.js
@@ -1,6 +1,6 @@
 /*
   You are given a program that logs pairings between mentors and students
-  It fails because the array `pairsById` can contain null values
+  It fails because the array `pairsByIndex` can contain null values
   It is decided that if there is a null value the program should exit
   - Add a check for null values, and if one exists, exit the program
   - Do not edit any of the existing code
@@ -12,10 +12,19 @@ let pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
 
+//if(pairsByIndex.some(element => element === null)) {
+ // console.log('Exiting...');
+ // process.exit(1);
+//}  
+
+
 let students = ["Islam", "Lesley", "Harun", "Rukmini"];
 let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
-let pairs = pairsByIndex.map(function (indexes) {
+let pairs = pairsByIndex
+.filter(element => element !== null)
+.map(function (indexes) {
+
   let student = students[indexes[0]];
   let mentor = mentors[indexes[1]];
   return [student, mentor];
