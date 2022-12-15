41f545415552d356e70827a70d3b8b42162ab97e
OleksChep <aachep@gmail.com>
2022-12-15 18:12:51 +0000

    F-array-forEach done

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,15 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(function format(element) {
+  if(element % 3 === 0) {
+    console.log("Fizz");
+  }else if (element % 5 === 0) {
+    console.log((element.toString()).replace(element, "Buzz"));
+  }else if (element % 3 === 0 && element % 5 === 0 ) {
+    console.log("FizzBuzz");
+  }else console.log(element);
+})
 /* EXPECTED OUTPUT */
 
 /*
