4fe4e77c5a88de811148a201c7c3a450ae35e181
Natalie Zablotska <113237479+nataliiazab@users.noreply.github.com>
2022-12-16 22:33:38 +0000

    2-mandatory-6 final function is not working properly

--- 2-mandatory/6-journey-planner.js
+++ 2-mandatory/6-journey-planner.js
@@ -20,8 +20,8 @@
 function checkCodeIsThere(stringText) {
   let magicWord = "code";
   //edit code below
-  if (stringText) {
-    return stringText;
+  if (stringText.includes(magicWord)) {
+    return stringText.indexOf(magicWord);
   } else {
     return "Not found";
   }
@@ -64,7 +64,9 @@ function checkCodeIsThere(stringText) {
 
   Hint: Use the corresponding array method to split the array.
 */
-function getTransportModes() {}
+function getTransportModes(arr) {
+  return arr.slice(1);
+}
 
 /*
   Implement the function isAccessibleByTransportMode that
@@ -81,7 +83,12 @@ function getTransportModes() {}
 
   Hint: Use the corresponding array method to decide if an element is included in an array.
 */
-function isAccessibleByTransportMode() {}
+function isAccessibleByTransportMode(arr, str) {
+  if (arr.includes(str)) {
+    return true;
+  }
+  return false;
+}
 
 /*
   Implement the function getLocationName that
@@ -92,7 +99,9 @@ function isAccessibleByTransportMode() {}
    - Returns the name of the location
       e.g: "Tower Bridge"
 */
-function getLocationName() {}
+function getLocationName(arr) {
+  return arr[0];
+}
 
 /*
  We arrived at the final method. it won't take long if you use the previously implemented functions wisely.
@@ -121,10 +130,45 @@ function getLocationName() {}
 
   Advanced challange: try to use arrow function when invoking an array method.
 */
+// function checkCodeIsThere(stringText) {
+//   let magicWord = "code";
+//   //edit code below
+//   if (stringText.includes(magicWord)) {
+//     return stringText.indexOf(magicWord);
+//   } else {
+//     return "Not found";
+//   }
+// }
+
+// function getTransportModes(arr) {
+//   return arr.slice(1);
+// }
+// function isAccessibleByTransportMode(arr, str) {
+//   if (arr.includes(str)) {
+//     return true;
+//   }
+//   return false;
+// }
+// function getLocationName(arr) {
+//   return arr[0];
+// }
+
 function journeyPlanner(locations, transportMode) {
-  // Implement the function body
+  let newArr = [];
+  if (locations.includes(transportMode)) {
+    newArr.push(locations.slice(1));
+  }
+  return newArr;
 }
 
+// const londonLocations = [
+//   ["Angel", "tube", "bus"],
+//   ["London Bridge", "tube", "river boat"],
+//   ["Tower Bridge", "tube", "bus"],
+//   ["Greenwich", "bus", "river boat"],
+// ];
+
+// console.log(journeyPlanner(londonLocations, "tube"));
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const string1 = "I Love coding and perfect code makes me happy";
