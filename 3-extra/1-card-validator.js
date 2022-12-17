1fcfdaef3d4695b1b3d2181da2365182ae221638
Farzaneh Haghani <68525882+farzaneh-haghani@users.noreply.github.com>
2022-12-17 04:45:29 +0000

    minor update

--- 3-extra/1-card-validator.js
+++ 3-extra/1-card-validator.js
@@ -5,7 +5,7 @@ function validator(cardNum) {
         cardNum[cardNum.length - 1] % 2 === 0 &&                                                //check for final digit be even
         cardNum.match(regex).length === 16 &&                                                   //check for all of them be numbers
         cardNum.match(regex).filter(value => value !== cardNum.match(regex)[0]).length > 0 &&   //check for having at least two different digits
-        cardNum.match(regex).reduce((sum, value) => sum + Number(value), 0) > 16);              //check for sum of all digits be greater than 16
+        cardNum.match(regex).reduce((sum, value) => sum + +(value), 0) > 16);              //check for sum of all digits be greater than 16
 
 }
 
