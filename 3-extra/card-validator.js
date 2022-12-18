4b8471f0f4c1d864213ff227504e5d711ad1c69d
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2022-12-18 10:22:43 +0000

    deleted the variable initialValue and add its value instead to .reduce

--- 3-extra/card-validator.js
+++ 3-extra/card-validator.js
@@ -1,6 +1,5 @@
 function cardValidator(cardNumber) {
   let cardNumberArray = cardNumber.toString().split(""); //this will create an array with each the digit as an element
-  const initialValue = 0; // initial value for for the accumulator in reduce method
   return (
     cardNumberArray.every((digit) => digit >= "0" && digit <= "9") && //check if the digit between 0 - 9
     cardNumberArray.length === 16 && // check the length of the card number equal to '16'
@@ -8,17 +7,17 @@ function cardValidator(cardNumber) {
     cardNumberArray[cardNumberArray.length - 1] % 2 === 0 && //check if the final number is even
     cardNumberArray
       .map((digit) => +digit) // convert the digits from string to number in order to use .reduce method
-      .reduce(  // calculate the sum of the array
+      .reduce(
+        // calculate the sum of the array
         (accumulator, currentValue) => accumulator + currentValue,
-        initialValue
+        0
       ) > 16 // check if the sum is greater than 16
   );
 }
 
 console.log(cardValidator(9999777788880000));
 console.log(cardValidator(6666666666661666));
-console.log(cardValidator('a92332119c011112'));
+console.log(cardValidator("a92332119c011112"));
 console.log(cardValidator(4444444444444444));
 console.log(cardValidator(1111111111111110));
 console.log(cardValidator(6666666666666661));
-
