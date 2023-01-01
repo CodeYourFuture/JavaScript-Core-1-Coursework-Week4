433a6181019b6e0f07ddae1bb0864ba48bd868fa
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2023-01-01 17:18:20 +0000

    exercises/F-array-forEach completed!

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,21 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+function formatNumber(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+}
+arr.map(formatNumber).forEach(function (number) {
+  console.log(number);
+});
+
 /* EXPECTED OUTPUT */
 
 /*
