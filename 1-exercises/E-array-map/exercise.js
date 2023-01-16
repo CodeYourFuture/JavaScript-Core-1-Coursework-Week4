218b00b0132a6b2d612b303e43f47f48ca92e69e
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-01-16 20:23:21 +0000

    exercises D, E, F

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -3,7 +3,9 @@
 
 let numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
-let numbersMultipliedByOneHundred; // complete this statement
+let numbersMultipliedByOneHundred = numbers.map((num) => {
+  return num * 100;
+}); // complete this statement
 
 console.log(numbersMultipliedByOneHundred);
 
