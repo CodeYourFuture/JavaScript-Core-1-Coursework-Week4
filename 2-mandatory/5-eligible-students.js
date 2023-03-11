348dea36bc2bacc9dd37287f528bc9637c5c690f
Appolin Semegni Fotso <108607571+AppolinFotso@users.noreply.github.com>
2023-03-11 19:35:53 +0000

    Passed the test

--- 2-mandatory/5-eligible-students.js
+++ 2-mandatory/5-eligible-students.js
@@ -7,7 +7,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function getEligibleStudents() {}
+function getEligibleStudents(arr) {
+  const newArr = [];
+  for (let name of arr) {
+    if (name[1] >= 8) {
+      newArr.push(name[0]);
+    }
+  }
+  return newArr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
