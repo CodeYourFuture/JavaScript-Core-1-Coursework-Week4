9eefaa753901efa9cd0641911529f4b2ad49d5ea
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-03-14 20:14:22 +0000

    8 done

--- 2-mandatory/8-password-validator.js
+++ 2-mandatory/8-password-validator.js
@@ -23,20 +23,36 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {}
+function validatePasswords(passwords) {
+  // Check if each password is valid
+  const result = passwords.map((password, index) => {
+    return (
+      password.length >= 5 &&
+      containsUppercaseLetter(password) &&
+      containsLowercaseLetter(password) &&
+      containsNumber(password) &&
+      containsSymbol(password) &&
+      !previousPasswords(password, index, passwords)
+    );
+  });
+  return result;
+}
 
 // Returns true if string contains at least one uppercase letter.
 function containsUppercaseLetter(string) {
-  return /[A-Z]/.test(string);
+  return string !== string.toLowerCase();
+   return /[A-Z]/.test(string);
 }
 
 // Returns true if string contains at least one lowercase letter.
 function containsLowercaseLetter(string) {
+  return string !== string.toUpperCase();
   return /[a-z]/.test(string);
 }
 
 // Returns true if string contains at least one number.
 function containsNumber(string) {
+  return /\d/.test(string);
   return /[0-9]/.test(string);
 }
 
