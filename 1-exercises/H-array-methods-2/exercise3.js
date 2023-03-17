5b52d39d966e0ce1ccd31e7947ecb7574e45094f
Shahid Amin <Shahidpythonbc@gmail.com>
2023-03-17 14:15:03 +0000

    Update exercise3.js

--- 1-exercises/H-array-methods-2/exercise3.js
+++ 1-exercises/H-array-methods-2/exercise3.js
@@ -7,7 +7,12 @@
 let ukNations = ["Scotland", "Wales", "England", "Northern Ireland"];
 
 function isInUK(country) {
-  return; // complete this statement
+if (ukNations.includes(country)){
+return true
+}
+ else{
+ return "Not a UK country"
+ }
 }
 
 /* 
