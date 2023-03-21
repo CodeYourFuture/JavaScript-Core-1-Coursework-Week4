c2681cc5722267fd5ec30552826df2679705942c
ZahraaTayyar <zahraatayyar@gmail.com>
2023-03-21 18:34:07 +0000

    mandatory-7 completed

--- 2-mandatory/test.js
+++ 2-mandatory/test.js
@@ -1,1 +1,13 @@
+function getLanes(streets) {
+  return streets.filter((street) => street.includes("Lane"));
+}
 
+const streetNames = [
+  "Abchurch Lane",
+  "Adam's Court",
+  "Addle Hill",
+  "Addle Lane",
+  "Alban Highwalk",
+];
+
+console.log(getLanes(streetNames));
